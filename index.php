f50240fab431bac7c265397f7cc9ff2ddd4920bb
DanielGomes97 <danielgommes1997@hotmail.com>
2022-11-30 01:53:41 -0300

    Adicionado o main.js vazio

--- index.php
+++ index.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="assets/css/main.css">
-	<title>Cardapio Online</title>
-</head>
-<body>
-	<section>
-		<?php include_once("header.php") ?>
-	</section>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="assets/css/main.css">
+		<title>Cardapio Online</title>
+	</head>
+	<body>
+		<section>
+			<?php include_once("header.php") ?>
+		</section>
 
-	<section>
-		<?php include_once("home.php") ?>
-	</section>
+		<section>
+			<?php include_once("home.php") ?>
+		</section>
 
-	<section>
-		<?php include_once("footer.php") ?>
-	</section>
+		<section>
+			<?php include_once("footer.php") ?>
+		</section>
 
-	<script src="assets/js/main.js"></script>
-</body>
+		<script src="assets/js/main.js"></script>
+	</body>
 </html>
